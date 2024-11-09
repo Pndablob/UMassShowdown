69f6755b7d9d3c0d558071be528597fe49edc205
Pndablob <64608661+Pndablob@users.noreply.github.com>
2024-11-09 01:40:52 -0500

    readded levels to Player class for level selector

--- src/Game/Player.ts
+++ src/Game/Player.ts
@@ -1,11 +1,17 @@
 class Player {
     private professors: Array<Professor>;
+    private levels: Array<number>; // levels unlocked by player
 
     constructor(professors: Array<Professor>, levels: Array<number>) {
         this.professors = professors;
+        this.levels = levels;
     }
 
     public getProfessors(): Array<Professor> {
         return this.professors;
+    }
+
+    public getLevels(): Array<number> {
+        return this.levels;
     }
 }
