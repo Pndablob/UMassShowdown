4e72f109f43a4ea37e030861ed53081f50cc94da
Shawn1515 <187825911+Shawn1515@users.noreply.github.com>
2024-11-09 17:56:50 -0500

    added Anderson move

--- src/Game/Professors/Anderson0.ts
+++ src/Game/Professors/Anderson0.ts
@@ -1,11 +1,12 @@
 import image from '../../Assets/Anderson.jpeg';
+import UCAReinforcements from '../Moves/UCAReinforcements';
 
 const prof = {
     name: "Emma Anderson",
     attack: 80,
     maxHealth: 250,
     defense: 50,
-    moves: [],
+    moves: [UCAReinforcements],
     picture: image,
     id: 0,
 }
