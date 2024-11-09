b79db3a7d3ba34126de6b2953f55515ff64c7419
Pndablob <64608661+Pndablob@users.noreply.github.com>
2024-11-09 01:58:49 -0500

    big changes to engine

--- src/Game/Game.ts
+++ src/Game/Game.ts
@@ -1,29 +1,85 @@
 import Dialogue from "../Dialogue/Dialogue";
+import Courses from "./Courses";
+import Player from "./Player";
+import Professor from "./Professor";
+import Action from "./Action";
 
 class Game {
     private dialogue: Dialogue;
     private player: Player;
-    // private opponent: Player;
+    private opponent: Player;
+    private actionQueue: Array<Action> = [];
+    private playerActiveProfessor: Professor;
+    private opponentActiveProfessor: Professor;
 
-    getPlayerProfessors() {
+    public getPlayerProfessors() {
         return this.player.getProfessors();
     }
 
-    updateState() {
-        // update state of battle
+    public getOpponentProfessors() {
+        return this.opponent.getProfessors();
     }
 
-    // getOpponentProfessors() {
-    //     return this.opponent.getProfessors();
-    // }
+    public getActiveProfessor() {
+        return this.playerActiveProfessor;
+    }
 
-    constructor(dialogue: Dialogue, player: Player, opponentID: number) {
+    public getOpponentActiveProfessor() {
+        return this.opponentActiveProfessor;
+    }
+
+    // UI sends engine an action
+    // engine processes action
+    // engine updates state of battle (player and opponent active professors), UI will check for changes in state
+    public processAction(action: Action): boolean {
+        // UI calls process action on an action
+        // engine adds player action to queue in a random order
+        // update state of battle based on random action (first action in queue)
+        // engine returns to UI whether there's actions left (bool)
+        // UI checks for dialogue / UI changes
+        // if there's moves left, UI calls update() again
+
+        return this.updateState()
+    }
+
+    public updateState(): boolean {
+        // check if there's any actions left
+        // if there's actions left, update state of battle
+        // return true if there's actions left
+
+        if (this.actionQueue.length != 0) {
+            let action = this.actionQueue.shift();
+            if (action && action.isPlayer) {
+                // process player action
+            } else {
+                // process opponent action
+            }
+            return true;
+        }
+
+        // no actions left
+        return false
+    }
+
+    constructor(dialogue: Dialogue, player: Player, courseID: string) {
         this.dialogue = dialogue;
         this.player = player;
         
-        // construct opponent: Player based on opponent ID
+        // construct opponent Player based on course ID
+        // get course based on course ID
+        let course = Courses.get(courseID);
+        
+        // catch nonexistent course
+        if (!course) {
+            throw new Error(`Course with ID ${courseID} not found`);
+        }
 
-        // call initBattle
+        // get professors from course
+        let professors = course.getProfessors();
+
+        this.opponent = new Player(professors, []);
+
+        // start battle by calling initBattle
     }
 
     initBattle() {
