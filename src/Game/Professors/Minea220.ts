d88f360ae6560063c4515a478430df6f0d683174
Pndablob <64608661+Pndablob@users.noreply.github.com>
2024-11-09 16:12:20 -0500

    testing 220

--- src/Game/Professors/Minea220.ts
+++ src/Game/Professors/Minea220.ts
@@ -1,11 +1,13 @@
 import image from '../../Assets/Minea.jpeg';
+import Exam from "../Moves/Exam";
+import Abstraction from "../Moves/Abstraction";
 
 const prof = {
     name: "Marius Minea",
     attack: 0,
     health: 0,
     maxHealth: 0,
-    moves: [],
+    moves: [Exam, Abstraction],
     picture: image,
     id: 9,
 }
