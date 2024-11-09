8acf626e54909d9106e02dcd946a31504b81b47c
Eric Gu <guja@vl965-172-31-107-114.wireless.umass.edu>
2024-11-09 00:02:49 -0500

    added css to level select screen

--- src/LevelSelect/LevelSelect.tsx
+++ src/LevelSelect/LevelSelect.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ImageCarousel from './ImageCarousel';
 import RetroButton from '../RetroButton/RetroButton';
+import './LevelSelect.css'
 
 function LevelSelect({globalState, setGlobalState}: any) {
   return (
     <div>
-        <h1>UMass Showdown</h1>
+        <h1 className='level-select-title'>UMass Showdown</h1>
         <ImageCarousel/>
-        <RetroButton message='load'/>
-        <RetroButton message='save'/>
+        <RetroButton message='load' className='load-button'/>
+        <br/>
+        <RetroButton message='save' className='save-button'/>
     </div>
   );
 }
