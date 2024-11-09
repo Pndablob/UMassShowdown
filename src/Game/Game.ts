a3ace5ef12c355ffbfd6a5362ad4f84c6c12e84e
Pndablob <64608661+Pndablob@users.noreply.github.com>
2024-11-09 02:16:46 -0500

    e

--- src/Game/Game.ts
+++ src/Game/Game.ts
@@ -12,55 +12,6 @@ class Game {
     private playerActiveProfessor: Professor;
     private opponentActiveProfessor: Professor;
 
-    public getPlayerProfessors() {
-        return this.player.getProfessors();
-    }
-
-    public getOpponentProfessors() {
-        return this.opponent.getProfessors();
-    }
-
-    public getActiveProfessor() {
-        return this.playerActiveProfessor;
-    }
-
-    public getOpponentActiveProfessor() {
-        return this.opponentActiveProfessor;
-    }
-
-    // UI sends engine an action
-    // engine processes action
-    // engine updates state of battle (player and opponent active professors), UI will check for changes in state
-    public processAction(action: Action): boolean {
-        // UI calls process action on an action
-        // engine adds player action to queue in a random order
-        // update state of battle based on random action (first action in queue)
-        // engine returns to UI whether there's actions left (bool)
-        // UI checks for dialogue / UI changes
-        // if there's moves left, UI calls update() again
-
-        return this.updateState()
-    }
-
-    public updateState(): boolean {
-        // check if there's any actions left
-        // if there's actions left, update state of battle
-        // return true if there's actions left
-
-        if (this.actionQueue.length != 0) {
-            let action = this.actionQueue.shift();
-            if (action && action.isPlayer) {
-                // process player action
-            } else {
-                // process opponent action
-            }
-            return true;
-        }
-
-        // no actions left
-        return false
-    }
-
     constructor(dialogue: Dialogue, player: Player, courseID: string) {
         this.dialogue = dialogue;
         this.player = player;
@@ -80,6 +31,65 @@ class Game {
         this.opponent = new Player(professors, []);
 
         // start battle by calling initBattle
+    }
+
+    public getPlayerProfessors() {
+        return this.player.getProfessors();
+    }
+
+    public getOpponentProfessors() {
+        return this.opponent.getProfessors();
+    }
+
+    public getActiveProfessor() {
+        return this.playerActiveProfessor;
+    }
+
+    public getOpponentActiveProfessor() {
+        return this.opponentActiveProfessor;
+    }
+
+    // UI sends engine an action
+    // engine processes action
+    // engine updates state of battle (player and opponent active professors) for player and opponent action
+    // UI will check for changes in state for each action
+    public processAction(action: Action): boolean {
+        // UI calls process action on an action
+        // engine adds player action to queue in a random order
+        // update state of battle based on random action (first action in queue)
+        // engine returns to UI whether there's actions left (bool)
+        // UI checks for dialogue / UI changes
+        // if there's moves left, UI calls update() again
+
+        return this.updateState()
+    }
+
+    public updateState(): boolean {
+        // check if there's any actions left
+        // if there's actions left, update state of battle
+        // return true if there's actions left
+
+        if (this.actionQueue.length != 0) {
+            let action = this.actionQueue.shift();
+            if (action && action.isPlayer) {
+                // process player action ==> attack opponent active professor
+            } else {
+                // process opponent action ==> attack player active professor
+            }
+
+            return true;
+        }
+
+        // no actions left
+        return false
+    }
+
+    attack(receiver: Player, attacker: Player, move: string) {
+        // get attacker active professor
+        // get receiver active professor
+        // get move from attacker active professor
+        // calculate damage = power * attack * critical chance * random modifier
+        // apply damage to receiver active professor
     }
 
     initBattle() {
