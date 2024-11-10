f0281d8f67a4774c24b25d8f08e8af9cf9526eef
Pndablob <64608661+Pndablob@users.noreply.github.com>
2024-11-09 22:40:00 -0500

    new profs

--- src/Game/Professors/Minea220.ts
+++ src/Game/Professors/Minea220.ts
@@ -1,15 +1,15 @@
 import image from '../../Assets/Minea.jpeg';
-import Abstraction from "../Moves/Abstraction";
 import RecursiveRampage from '../Moves/RecursiveRampage';
 import MariusExam from '../Moves/MariusExam';
 import JavaScriptJab from '../Moves/JavaScriptJab';
+import UCAReinforcements from '../Moves/UCAReinforcements';
 
 const prof = {
     name: "Marius Minea (220)",
     attack: 70,
     maxHealth: 220,
     defense: 60,
-    moves: [Abstraction, RecursiveRampage, MariusExam, JavaScriptJab],
+    moves: [UCAReinforcements, RecursiveRampage, MariusExam, JavaScriptJab],
     picture: image,
     id: 9,
 }
