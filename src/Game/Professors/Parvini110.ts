83edff16092346fee215977ff6043925590abeee
Pndablob <64608661+Pndablob@users.noreply.github.com>
2024-11-09 22:50:34 -0500

    add parvini 110

---
+++ src/Game/Professors/Parvini110.ts
@@ -0,0 +1,16 @@
+import image from '../../Assets/Parvini.jpeg';
+import Exam from '../Moves/Exam';
+import Intractability from '../Moves/Intractability';
+import RecursiveRampage from '../Moves/RecursiveRampage';
+
+const prof = {
+    name: "Ghazaleh Parvini",
+    attack: 50,
+    maxHealth: 160,
+    defense: 80,
+    moves: [Exam, RecursiveRampage, Intractability],
+    picture: image,
+    id: 11,
+}
+
+export default prof;
